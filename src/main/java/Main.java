cc80d1af41753ab6566b37ede2caca6221b333b7
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-01-24 16:04:57 -0400

    ci: :construction: add connection to database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import database.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection.getConnection().getMongodb();
     }
 }
