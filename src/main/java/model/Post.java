cb7a8da8e245e970a3b9f8bcdb96bdab09160c22
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-01-27 19:47:11 -0400

    ci: :construction: add model to Post

---
+++ src/main/java/model/Post.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.util.Date;
+
+public class Post {
+
+    private String title;
+    private String description;
+    private Date dateCreated;
+    private int likes;
+    private String idUser;
+
+    public Post(String title, String description, Date dateCreated, String idUser) {
+        this.title = title;
+        this.description = description;
+        this.dateCreated = dateCreated;
+        this.idUser = idUser;
+        this.likes = 0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+}
